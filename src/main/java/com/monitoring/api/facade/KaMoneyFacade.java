359937fd3958ae9cf189e7ac48fecc34a7c67f3d
young891221 <young891221@gmail.com>
2018-03-27 23:34:46 +0900

    기능: Rule 통합 테스트 준비

--- src/main/java/com/monitoring/api/facade/KaMoneyFacade.java
+++ src/main/java/com/monitoring/api/facade/KaMoneyFacade.java
@@ -4,9 +4,7 @@ import com.monitoring.api.domain.Account;
 import com.monitoring.api.domain.KaMoney;
 import com.monitoring.api.domain.User;
 import com.monitoring.api.domain.log.KaMoneyEventLog;
-import com.monitoring.api.rule.RuleA;
-import com.monitoring.api.rule.RuleEngine;
-import com.monitoring.api.rule.RuleList;
+import com.monitoring.api.rule.*;
 import com.monitoring.api.service.KaMoneyEventLogService;
 import com.monitoring.api.service.KaMoneyService;
 import com.monitoring.api.service.RuleLogService;
@@ -15,6 +13,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by young891221@gmail.com on 2018-03-26
@@ -34,23 +33,53 @@ public class KaMoneyFacade {
         this.ruleLogService = ruleLogService;
     }
 
+    /**
+     * 계좌 개설 기능
+     * @param user
+     * @param account
+     */
     public void openKaMoney(final User user, final Account account) {
         KaMoney kaMoney = kaMoneyService.openKaMoney(user, account);
         kaMoneyEventLogService.saveLog(KaMoneyEventLog.openKaMoney(kaMoney));
     }
 
+    /**
+     * 충전 기능
+     * @param user
+     * @param money
+     */
     public void chargeKaMoney(final User user, final long money) {
         KaMoney beforeKaMoney = kaMoneyService.findByUser(user);
         KaMoney afterKaMoney = kaMoneyService.chargeMoney(user, money);
         kaMoneyEventLogService.saveLog(KaMoneyEventLog.cargeKaMoney(beforeKaMoney, afterKaMoney));
     }
 
+    /**
+     * 받기 기능
+     * @param toUser
+     * @param money
+     */
     public void receiveKaMoney(final User toUser, final long money) {
         KaMoney beforeKaMoney = kaMoneyService.findByUser(toUser);
         KaMoney afterKaMoney = kaMoneyService.receiveKaMoney(beforeKaMoney, money);
         kaMoneyEventLogService.saveLog(KaMoneyEventLog.receiveKaMoney(beforeKaMoney, afterKaMoney));
+
+        List<KaMoneyEventLog> ruleBlogs = kaMoneyEventLogService.findByCreatedDateAfterAndUser(LocalDateTime.now().minusDays(7), afterKaMoney.getUser());
+        final LocalDateTime ruleCTime = LocalDateTime.now().minusHours(2);
+        List<KaMoneyEventLog> ruleClogs = ruleBlogs.stream()
+                .filter(log -> ruleCTime.isBefore(log.getCreatedDate()))
+                .collect(Collectors.toList());
+        RuleList ruleList = RuleList.generateByArray(RuleB.create(ruleBlogs), RuleC.create(ruleClogs));
+        RuleEngine ruleEngine = new RuleEngine(ruleList);
+        ruleLogService.saveRules(ruleEngine.run(), afterKaMoney.getUser());
     }
 
+    /**
+     * 송금 기능
+     * @param fromUser
+     * @param toUser
+     * @param money
+     */
     public void remittanceKaMoney(final User fromUser, final User toUser, final long money) {
         KaMoney beforeKaMoney = kaMoneyService.findByUser(fromUser);
         KaMoney afterKaMoney = kaMoneyService.remittanceMoney(beforeKaMoney, money);
--- src/test/java/com/monitoring/api/rule/RuleIntegrationTest.java
+++ src/test/java/com/monitoring/api/rule/RuleIntegrationTest.java
@@ -1,9 +1,30 @@
 package com.monitoring.api.rule;
 
 import com.monitoring.api.AcceptanceTest;
+import com.monitoring.api.facade.KaMoneyFacade;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by KimYJ on 2018-03-27.
  */
 public class RuleIntegrationTest extends AcceptanceTest {
+
+    @Autowired
+    private KaMoneyFacade kaMoneyFacade;
+
+    @Test
+    public void RuleA_통합_테스트() {
+
+    }
+
+    @Test
+    public void RuleB_통합_테스트() {
+
+    }
+
+    @Test
+    public void RuleC_통합_테스트() {
+
+    }
 }
